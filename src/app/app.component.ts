166c4bd2cbb5654f3f8290ee98ca4f8604a6d684
Gabriel Pereira Mendes <gabriel.mendes@montreal.com.br>
2024-10-17 12:51:03 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngstyle';
+  fontSize: number = 15;
+  textColor: 'red' | 'blue' = 'red';
+  buttonText: 'Vermelho' | 'Azul' = 'Azul';
+  stylesString: string = 'font-size: 15px; color: red';
+  stylesObj = {
+    'font-size': this.fontSize + 'px',
+    'color': this.textColor
+  };
+
+  increaseFontSize() { 
+    this.fontSize +=5;
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    };
+  }
+
+  toggleFontColor() {
+    if(this.textColor === 'red') {
+      this.textColor = 'blue';
+      this.buttonText = 'Vermelho';
+    } else {
+      this.textColor = 'red';
+      this.buttonText = 'Azul';
+    }
+
+    this.stylesString = `font-size: ${this.fontSize}px; color: ${this.textColor};`;
+
+    this.stylesObj = {
+      'font-size': this.fontSize + 'px',
+      'color': this.textColor
+    };
+  }
+  
 }
